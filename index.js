fa6ae0b07669572e3528813cb6c3e4d6699ad433
FranckLdx <franck.ledoux.pro@gmail.com>
2016-12-02 22:47:34 +0100

    Ad test

--- index.js
+++ index.js
@@ -1,53 +1,77 @@
 const del = require('del');
 const gulp = require('gulp');
+const gutil = require('gulp-util');
 const sourcemaps = require('gulp-sourcemaps');
 const typeScript = require('gulp-typeScript');
 const path = require('path');
+const through2 = require('through2');
 
 class TsConfig {
-    constructor(tsConfigLocation) {
-      this._location = path.dirname(path.resolve('.', tsConfigLocation));
-      this._tsConfig = require(tsConfigLocation);
+  constructor(tsConfigLocation) {
+    this._tsConfig = require(tsConfigLocation);
+  }
+
+  get tsDir() { return `${this._tsConfig.rootDir}`; }
+
+  get tsFiles() {
+    const includes = this._tsConfig.include || ['/**/*.ts'];
+    return includes.map(include => path.join(this.tsDir, include));
+  }
+
+  get jsDir() { return `${this._tsConfig.compilerOptions.outDir}`; }
+
+  get jsFiles() { return `${this.jsDir}/**/*.js`; }
+
+  get mapFiles() { return `${this.jsDir}/**/*.map`; }
+
+  get ambiantFiles() { return `${this.jsDir}/**/*.d.ts`; }
+
+  get compilerOptions() { return this._tsConfig.compilerOptions; }
+
+  get isSourceMap() { return this._tsConfig.compilerOptions.sourceMap || false; }
+
+  cleanTask() {
+    return del([this.jsFiles, this.mapFiles, this.ambiantFiles]);
+  }
+
+  buildTask() {
+    const isSourcemap = this.isSourceMap;
+    let pipe = gulp.src(this.tsFiles);
+    if (isSourcemap) {
+      pipe = pipe.pipe(sourcemaps.init());
     }
-
-    get tsDir() { return `${this._location}/${this._tsConfig.rootDir}`; }
-
-    get tsFiles() { return `${this.tsDir}/**/*.ts`; }
-
-    get jsDir() { return `${this._location}/${this._tsConfig.compilerOptions.outDir}`; }
-
-    get jsFiles() { return `${this.jsDir}/**/*.js`; }
-
-    get mapFiles() { return `${this.jsDir}/**/*.map`; }
-
-    get ambiantFiles() { return `${this.jsDir}/**/*.d.ts`; }
-
-    get compilerOptions() { return this._tsConfig.compilerOptions; }
-
-    get isSourceMap() { return this._tsConfig.compilerOptions.sourceMap || false; }
-
-    cleanBuildTask() {
-      return del([this.jsFiles, this.mapFiles, this.ambiantFiles]);
+    pipe = pipe.pipe(typeScript(this.compilerOptions));
+    if (isSourcemap) {
+      pipe = pipe.pipe(sourcemaps.write('.'));
     }
-
-    buildTask() {
-      const isSourcemap = this.isSourceMap;
-      let pipe = gulp.src(this.tsFiles);
-      if (isSourcemap) {
-        pipe = pipe.pipe(sourcemaps.init());
-      }
-      pipe = pipe.pipe(typeScript(this.compilerOptions))
-      if (isSourcemap) {
-        pipe = pipe.pipe(sourcemaps.write('.'));
-      }
-      return pipe.pipe(gulp.dest(this.jsDir));
-    }
+    return pipe.pipe(gulp.dest(this.jsDir));
+  }
 }
 
-module.exports = (inputFile) = {
-  const tsConfig = new TsConfig(inputFile);
-  return {
-    build: tsConfig.buildTask(),
-    clean: tsConfig.cleanTask(),
-  }
+function doSomething(transform) {
+  let gotAfile = false;
+  return through2.obj(function toStream(file, encoding, callback) {
+    gotAfile = true;
+    this.push(transform(file));
+    callback();
+  }).on('finish', () => {
+    if (!gotAfile) {
+      gutil.log(gutil.colors.red('Got no config file'));
+    }
+  });
+}
+
+module.exports.clean = () => {
+  return doSomething((file) => {
+    const tsConfig = new TsConfig(file.path);
+    return tsConfig.cleanTask();
+  });
+};
+
+module.exports.build = () => {
+  return doSomething((file) => {
+    const tsConfig = new TsConfig(file.path);
+    gutil.log(gutil.colors.red(tsConfig.tsDir));
+    return tsConfig.buildTask();
+  });
 };
