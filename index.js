2151e837af739724dd54357d1066f18d119a5d92
FranckLdx <franck.ledoux.pro@gmail.com>
2016-12-18 22:50:48 +0100

    Fix task ending

--- index.js
+++ index.js
@@ -7,8 +7,19 @@ function getTransformPipe(transform) {
   let gotAfile = false;
   return through2.obj(function toStream(file, encoding, callback) {
     gotAfile = true;
-    this.push(transform(file));
-    callback();
+    const transformer = transform(file);
+    if (transformer instanceof Promise) {
+      this.push(transform(file));
+      callback();
+      return;
+    }
+    transformer.on('data', (data) => {
+      this.push(data);
+    }).on('end', () => {
+      callback();
+    }).on('error', (error) => {
+      callback(error);
+    });
   }).on('finish', () => {
     if (!gotAfile) {
       gutil.log(gutil.colors.red('Got no config file'));
