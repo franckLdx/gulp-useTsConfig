b96994c43da2ec0eab407f085124bbd8638f1219
franck ledoux <franck.ledoux.pro@gmail.com>
2018-02-17 16:24:33 +0100

    Examples refactoring

---
+++ example/watch/gulpfile.js
@@ -0,0 +1,22 @@
+
+const gulp = require('gulp');
+const useTsConfig = require('../../index.js');
+
+const tsConfig = './tsconfig.json';
+
+gulp.task('pre-build', () => {
+  return gulp.src(tsConfig)
+    .pipe(useTsConfig.clean());
+});
+
+gulp.task('build', ['pre-build'], () => {
+  return gulp.src(tsConfig)
+    .pipe(useTsConfig.build());
+});
+
+gulp.task('watch', ['build'], () => {
+  return gulp.src(tsConfig)
+    .pipe(useTsConfig.watch());
+});
+
+gulp.task('default', ['watch']);
